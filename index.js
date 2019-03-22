81906f34d0ad55312d7fb5916e9ce1d1ca188f35
Trey Foster <treybfoster@gmail.com>
2019-03-22 07:36:18 -0500

    Init Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+export default () => {
+  let hour = new Date().getHours();
+  if (hour < 12) {
+    return "morning";
+  }
+  if (hour > 12 && hour < 17) {
+    return "afternoon";
+  }
+  if (hour > 17) {
+    return "evening";
+  }
+  if (hour < 0) {
+    return "hell";
+  }
+};
